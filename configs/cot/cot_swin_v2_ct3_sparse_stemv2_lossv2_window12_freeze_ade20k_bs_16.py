e945fe74b8b0ccbf35dd074009962d1c4f4f337b
yilinshao <yl_shao@outlook.com>
2022-11-24 16:25:52 +0800

    test: slide test ade20K

--- configs/cot/cot_swin_v2_ct3_sparse_stemv2_lossv2_window12_freeze_ade20k_bs_16.py
+++ configs/cot/cot_swin_v2_ct3_sparse_stemv2_lossv2_window12_freeze_ade20k_bs_16.py
@@ -22,7 +22,7 @@ model = dict(
               num_classes=150
               ),
     decode_head=dict(num_classes=150),
-    # test_cfg=dict(mode='slide', crop_size=(512, 512), stride=(341, 341)),
+    test_cfg=dict(mode='slide', crop_size=(512, 512), stride=(341, 341)),
     auxiliary_head=[
         dict(
             type='FCNHead',
